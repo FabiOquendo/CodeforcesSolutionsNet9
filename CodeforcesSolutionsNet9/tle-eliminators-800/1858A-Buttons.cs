5d942c960ce54980adba0a683c10f5af1391bde4
Fabio Oquendo <fabio.oquendo@alithya.com>
2026-05-13 10:50:12 -0400

    Solution to Problem 1858A-Buttons

---
+++ CodeforcesSolutionsNet9/tle-eliminators-800/1858A-Buttons.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+using var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+Console.SetOut(sw);
+
+var sc = new Scanner(Console.OpenStandardInput());
+
+int t = sc.NextInt();
+
+while (t-- > 0)
+{
+    long a = sc.NextLong();
+    long b = sc.NextLong();
+    long c = sc.NextLong();
+
+    string answer = (b > a || a == b && c % 2 == 0) ? "Second" : "First";
+
+    sw.WriteLine(answer);
+}
+
+Console.Out.Flush();
+
+public class Scanner
+{
+    private readonly StreamReader _reader;
+    private string[] _tokens = [];
+    private int _index;
+
+    public Scanner(Stream stream) => _reader = new StreamReader(stream);
+
+    public string Next()
+    {
+        while (_index >= _tokens.Length)
+        {
+            var line = _reader.ReadLine();
+            if (line == null) return null;
+            _tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            _index = 0;
+        }
+        return _tokens[_index++];
+    }
+
+    public int NextInt() => int.Parse(Next());
+    public long NextLong() => long.Parse(Next());
+    public double NextDouble() => double.Parse(Next(), CultureInfo.InvariantCulture);
+}
